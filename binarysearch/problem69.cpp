efe2f9cd7d4f4c236f5ff2703b1c24d967019735
ujjwal026 <154792613+ujjwal026@users.noreply.github.com>
2024-05-10 14:27:50 +0530

    Update problem69.cpp

--- binarysearch/problem69.cpp
+++ binarysearch/problem69.cpp
@@ -1,14 +1,31 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        left = 0
-        right = x
-        while left <= right:
-            mid = (left + right) // 2
-            if mid * mid < x:
-                left = mid + 1
-            elif mid * mid > x:
-                right = mid -1
-            else:
-                return mid
-            
-        return right
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int floorSqrt(int n) {
+    int low = 1, high = n;
+    //Binary search on the answers:
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long val = mid * mid;
+        if (val <= (long long)(n)) {
+            //eliminate the left half:
+            low = mid + 1;
+        }
+        else {
+            //eliminate the right half:
+            high = mid - 1;
+        }
+    }
+    return high;
+}
+
+int main()
+{
+    int n = 28;
+    int ans = floorSqrt(n);
+    cout << "The floor of square root of " << n
+         << " is: " << ans << "\n";
+    return 0;
+}
+
