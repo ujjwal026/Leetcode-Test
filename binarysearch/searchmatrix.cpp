29510484cb2582756856a40adce75d993685dd03
ujjwal026 <154792613+ujjwal026@users.noreply.github.com>
2024-05-17 01:08:37 +0530

    Update searchmatrix.cpp

--- binarysearch/searchmatrix.cpp
+++ binarysearch/searchmatrix.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size(),
-			cols = matrix[0].size(),
-            row = 0, col = cols - 1;
+        int n = matrix.size(),
+			m= matrix[0].size(),
+        i= 0, j = m- 1;
 			
-        while (row < rows && col > -1) {
-            int cur = matrix[row][col];
+        while (i < n && j > -1) {
+            int cur = matrix[i][j];
             if (cur == target) return true;
-            if (target > cur) row++;
-            else col--;
+            if (target > cur) i++;
+            else j--;
         }
         
         return false;
